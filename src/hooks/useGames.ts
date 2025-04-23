a66a6fe5d15a046702de790debc7dcaa90f1e5f6
Krishna Purohith <purohithkrishna.1@gmail.com>
2025-04-23 21:51:43 +0530

    Refactor- manage state with zustand

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,8 +1,8 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
-import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
 import ms from "ms";
+import useGameQueryStore from "../store";
 
 const apiClient = new APIClient<Game>('/games');
 
@@ -16,8 +16,10 @@ export interface Game {
 
 }
 
-const useGames = (gameQuery: GameQuery) => 
-  useInfiniteQuery<FetchResponse<Game>, Error>({
+
+const useGames = () => {
+  const gameQuery = useGameQueryStore(s => s.gameQuery);
+  return useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ['games', gameQuery],
     queryFn: ({ pageParam = 1 }) => 
       apiClient.getAll({
@@ -35,6 +37,7 @@ const useGames = (gameQuery: GameQuery) =>
     }
     
   })
+}
 
 
 export default useGames;
